85a6810cba20cbc3b212a39aa91d1e056a2d4584
161PC <161PC@DESKTOP-GKJ0FR7>
2022-10-18 17:49:15 +0300

    Добавьте файлы проекта.

---
+++ FirstClass/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    internal class Person
+    {
+        public string _FullName = "Andry";
+        public int _Age = 21;
+        public void Move()
+        {
+            Console.WriteLine($"{_FullName} сделал шаг");
+        }
+        public void Talk()
+        {
+            Console.WriteLine($"{_FullName} говорит - Ананас");
+        }
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,7 @@
+﻿using FirstClass;
+Person Andry = new Person();
+Andry.Move();
+Person Andry2 = new Person();
+string personName = Andry2._FullName;
+int age = Andry2._Age;
+Andry2.Talk();
